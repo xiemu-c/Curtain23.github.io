dc5fb5e21a66a6f6f4cfb13bbf9ded742b550c6a
Curtain23 <1041291@qq.com>
2024-07-29 10:35:40 +0800

    Site updated: 2024-07-29 10:35:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/5c83816a.html","posts/cdd8f365.html","posts/126b2990.html"];function toRandomPost(){
+var posts=["posts/cdd8f365.html","posts/5c83816a.html","posts/126b2990.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
