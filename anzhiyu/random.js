c365c597c5b1654676ecd5421dd4f4d1a4158771
Curtain23 <1041291@qq.com>
2024-08-07 17:10:32 +0800

    Site updated: 2024-08-07 17:10:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/126b2990.html","posts/cdd8f365.html","posts/e0de93dd.html","posts/dd3fb5c9.html","posts/5c83816a.html"];function toRandomPost(){
+var posts=["posts/5c83816a.html","posts/cdd8f365.html","posts/e0de93dd.html","posts/126b2990.html","posts/dd3fb5c9.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
